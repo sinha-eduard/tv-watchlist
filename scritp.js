c7454ea65b17d0be734c4d64ba268552395c035b
sinha-eduard <77357095+sinha-eduard@users.noreply.github.com>
2024-09-11 20:35:07 -0400

    Error Handling
    Added some error handling and Image creating to app

--- scritp.js
+++ scritp.js
@@ -16,24 +16,35 @@ const search = async () =>{
     let show = searchBar.value
     let res = await getShow(show)
     let resArr = []
-    for(let i = 0; i<10; i++){
-        resArr.push(res.data[i])
+    for(let i = 0; i<res.data.length; i++){ 
+    resArr.push(res.data[i])
+       
     }
     return resArr
 }
 
 const displaySearch = async (res) => {
     searchDiv.innerHTML = ""
-    for(let i = 0; i<10; i++){
-        const newP = document.createElement('p');
-        newP.append(res[i].show.name)
-        searchDiv.append(newP)
+    for(let i = 0; i<res.length; i++){
+        const newDiv = document.createElement('div');
+        const image = document.createElement('img');
+        const title = document.createElement('p');
+        title.append(res[i].show.name)
+        try {
+            image.src = res[i].show.image.medium
+        } catch (error) {
+            image.src = "https://static.tvmaze.com/images/no-img/no-img-portrait-text.png"
+        }
+        newDiv.append(image)
+        newDiv.append(title)
+        searchDiv.append(newDiv)
     }   
 }
 
 searchForm.addEventListener('submit', async function(e){
     e.preventDefault()
     let res =  await search()
+    console.log(res)
     displaySearch(res)
     searchBar.value = '';
     
