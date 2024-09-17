fafbac03b5a25fe7d92ababafce17a4265606b49
sinha-eduard <77357095+sinha-eduard@users.noreply.github.com>
2024-09-17 16:59:49 -0400

    Added Styling using Tailwind

--- public/showRequests.js
+++ public/showRequests.js
@@ -1,6 +1,9 @@
 const searchBar = document.querySelector("#searchBar");
 const searchForm = document.querySelector("#searchForm");
 const searchDiv = document.querySelector("#searchResults");
+const searchText = document.querySelector("#searchText")
+const loadingDiv = document.querySelector("#loadingDiv")
+
 let shows = document.querySelectorAll(".results");
 
 const getShow = async (showName) => {
@@ -40,28 +43,34 @@ const displaySearch = async (res) => {
     const input = document.createElement("input");
 
     title.append(res[i].show.name);
+    title.classList.add("font-bold")
+    title.classList.add("text-xl")
     if (res[i].show.rating.average === null) {
       rating.append("N/A");
     } else {
       rating.append(res[i].show.rating.average);
     }
     status.append(res[i].show.status);
+
     try {
       image.src = res[i].show.image.medium;
-      
     } catch (error) {
       image.src =
         "https://static.tvmaze.com/images/no-img/no-img-portrait-text.png";
-      image.style.width = "210px"
     }
+
     let gen = res[i].show.genres;
     for (let element of gen) {
       genre.append(element + " ");
     }
 
     let id = res[i].show.id;
-    
-    input.type = "checkbox";
+
+    imgDiv.classList.add("h-[264px]")
+    imgDiv.classList.add("w-[188px]")
+    image.classList.add("max-h-[264px]")
+    image.classList.add("max-w-[188px]")
+    input.type = "radio";
     input.name = "showDes"
     newDiv.append(input)
     imgDiv.append(image);
@@ -72,10 +81,13 @@ const displaySearch = async (res) => {
     infoDiv.append(imgDiv);
     infoDiv.append(mainDiv);
     newDiv.append(infoDiv);
+    mainDiv.classList.add("pl-4")
     infoDiv.classList.add("collapse-title");
     infoDiv.classList.add("flex");
     newDiv.classList.add("collapse");
     newDiv.classList.add("w-2/5");
+    newDiv.classList.add("hover:bg-secondary-content");
+    newDiv.classList.add("text-white");
   
     
     mainDiv.classList.add("text-results");
@@ -120,11 +132,31 @@ const displaySearch = async (res) => {
   }
 };
 
+const setH2 = function(){
+  let show = searchBar.value;
+  searchText.innerText = `Search Results For ${show}`
+}
 
+const createLoading = function(){
+  const load = document.createElement("span");
+  load.classList.add("loading")
+  load.classList.add("loading-spinner")
+  load.classList.add("text-accent")
+  load.id="load-span"
+  loadingDiv.append(load)
+}
+
+const removeLoading = function(){
+  const loadSpan = document.querySelector("#load-span")
+  loadSpan.remove()
+}
 
 searchForm.addEventListener("submit", async function (e) {
   e.preventDefault();
+  setH2()
+  createLoading()
   let res = await search();
   await displaySearch(res);
+  removeLoading()
   searchBar.value = "";
 });
